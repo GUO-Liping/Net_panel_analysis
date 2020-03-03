a638e14d77ac5bf8d48a80fb859063b899b12ea3
GUO-Liping <liping0_0@my.swjtu.edu.cn>
2020-03-03 20:51:05 +0800

    In this version, constitutive equations of spring-fiber element are based on gama_Nmax

--- part_to_compute.py
+++ part_to_compute.py
@@ -11,67 +11,71 @@ Date: 2020/3/2
 Remark: 影响计算结果的因素还有：
 	(1)直线传力纤维与变形后的环网曲面传力路径之间的角度差异; 
 	(2)三环环链拉伸代表了一种网环受力的最不利情形，实际网片中传力路径上环网轴向应力发展程度可能高于环链试验值
+log: 该版本将3，4圈分为一组，将5~19分为一组
 '''
 
 import numpy as np
 
 def func_vector_x_direction(para_wx, para_mx, para_ax, para_wy, para_h):
-    index_xi = np.linspace(1, para_mx, para_mx, endpoint=True)
+	index_xi = np.linspace(1, para_mx, para_mx, endpoint=True)
 
-    xu = para_ax * (index_xi - 1/2)
-    yu = np.sqrt(Rp**2 - (para_ax*index_xi - para_ax/2)**2)
-    zu = np.zeros(para_mx) + para_h
-    xd = para_wx * (index_xi-1/2)/(2*para_mx + 1)
-    yd = np.zeros(para_mx) + para_wy/2
-    zd = np.zeros(para_mx) + 0
+	xu = para_ax * (index_xi - 1/2)
+	yu = np.sqrt(Rp**2 - (para_ax*index_xi - para_ax/2)**2)
+	zu = np.zeros(para_mx) + para_h
+	xd = para_wx * (index_xi-1/2)/(2*para_mx + 1)
+	yd = np.zeros(para_mx) + para_wy/2
+	zd = np.zeros(para_mx) + 0
 
-    length_element_xi = np.sqrt((xu-xd)**2 + (yu-yd)**2 + (zu-zd)**2)
-    return length_element_xi
+	length_element_xi = np.sqrt((xu-xd)**2 + (yu-yd)**2 + (zu-zd)**2)
+	return length_element_xi
 
 def func_vector_y_direction(para_wy, para_my, para_ay, para_wx, para_h):
-    index_yi = np.linspace(1, para_my, para_my, endpoint=True)
+	index_yi = np.linspace(1, para_my, para_my, endpoint=True)
 
-    xu = np.sqrt(Rp**2 - (para_ay*index_yi - para_ay/2)**2)
-    yu = para_ay * (index_yi - 1/2)
-    zu = np.zeros(para_my) + para_h
-    xd = np.zeros(para_my) + para_wx/2
-    yd = para_wy * (index_yi-1/2)/(2*para_my + 1)
-    zd = np.zeros(para_my) + 0
+	xu = np.sqrt(Rp**2 - (para_ay*index_yi - para_ay/2)**2)
+	yu = para_ay * (index_yi - 1/2)
+	zu = np.zeros(para_my) + para_h
+	xd = np.zeros(para_my) + para_wx/2
+	yd = para_wy * (index_yi-1/2)/(2*para_my + 1)
+	zd = np.zeros(para_my) + 0
 
-    length_element_y = np.sqrt((xu-xd)**2 + (yu-yd)**2 + (zu-zd)**2)
-    return length_element_y
+	length_element_y = np.sqrt((xu-xd)**2 + (yu-yd)**2 + (zu-zd)**2)
+	return length_element_y
 
 def func_ringChianDataFit(nw,sigma_y):
-    lN0 = 0.3*3
+	nw_array_all = np.array([3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 7, 7, 7, 9, 9, 9, 9, 9, 9,12, 12, 12, 16, 16, 16, 16, 16, 16, 19, 19, 19,])
+	gama_Nmax_all = np.array([0.24401, 0.38275, 0.27866, 0.28444, 0.34242, 0.30484, 0.36479,0.38741, 0.3253, 0.34, 0.36, 0.38, 0.42744, 0.40375, 0.36293, 0.35164, 0.31833, 0.40301,0.41, 0.38, 0.32, 0.39305, 0.36725, 0.34754, 0.49815, 0.38193, 0.45116, 0.4, 0.44, 0.5,0.45348, 0.43676, 0.431])
+	poly_gama_Nmax_all = np.polyfit(nw_array_all, gama_Nmax_all,1)
+	gama_N2 = np.polyval(poly_gama_Nmax_all, nw)
 
-    nw_array = np.array([3,4,5,7,9,12,16,19],dtype='float')
-    FN2_array = np.array([16.25e3,18.84e3,44.94e3,69.72e3,80.55e3,110.88e3,177.66e3,209.39e3],dtype='float')
-    delta_lN2_array = 0.001*np.array([543.68,535.67,534.59,534.92,521.44,522.54,517.36,507.92],dtype='float')
-    dmin1, dmin2 = 0.0022, 0.003
-    Area_array = np.pi/4*np.hstack((dmin1**2*nw_array[nw_array<5], dmin2**2*nw_array[nw_array>=5]))
-    gamaMax_array = FN2_array/(sigma_y*2*Area_array)
+	nw_array = np.array([3,4,5,7,9,12,16,19])
+	lN0 = 0.3*3
+	delta_lN2_array = 0.001*np.array([543.68,539.67,534.59,534.92,521.44,522.54,517.36,507.92],dtype='float')
+	poly_delta_lN2 = np.polyfit(nw_array, delta_lN2_array,1)
+	delta_lN2 = np.polyval(poly_delta_lN2, nw)
+	dmin34, dmin519 = 0.0022, 0.003
+	
+	if nw < 5:
+		after_fit_FN2 = gama_N2*sigma_y*2*nw*np.pi/4*dmin34**2
+	else:
+		after_fit_FN2 = gama_N2*sigma_y*2*nw*np.pi/4*dmin519**2
+	
+	after_fit_gamaN2 = gama_N2
+	after_fit_lN2 = lN0 + delta_lN2
 
-    poly_FN2_func = np.polyfit(nw_array, FN2_array,1)
-    poly_delta_lN2_func = np.polyfit(nw_array, delta_lN2_array,1)
-    poly_gamaMax_func = np.polyfit(nw_array, gamaMax_array,1)
+	after_fit_FN1 = after_fit_FN2*0.15
+	after_fit_lN1 = lN0 + delta_lN2*0.85
+	after_fit_gamaN2 = gama_N2
+	after_fit_gamaN1 = after_fit_gamaN2 * 0.15
 
-    after_fit_FN2 = np.polyval(poly_FN2_func, nw)
-    after_fit_delta_lN2 = np.polyval(poly_delta_lN2_func, nw)
-    after_fit_gamaMax = np.polyval(poly_gamaMax_func, nw)
-
-    after_fit_lN2 = lN0 + after_fit_delta_lN2
-    after_fit_FN1 = after_fit_FN2*0.15
-    after_fit_lN1 = lN0 + after_fit_delta_lN2*0.85
-    after_fit_gamaN1 = after_fit_gamaMax * 0.15
-
-    return after_fit_FN1, after_fit_FN2, lN0, after_fit_lN1, after_fit_lN2, after_fit_gamaN1, after_fit_gamaMax
+	return after_fit_FN1, after_fit_FN2, lN0, after_fit_lN1, after_fit_lN2, after_fit_gamaN1, after_fit_gamaN2
 
 def func_round(number):
-    if number % 1 == 0.5:
-        number = number + 0.5
-    else:
-        number = round(number)
-    return int(number)
+	if number % 1 == 0.5:
+	    number = number + 0.5
+	else:
+	    number = round(number)
+	return int(number)
 
 def func_choose_dmin(para_nw):
 	if para_nw < 5:
@@ -106,8 +110,10 @@ def compute_height(L0_x,K1_x,K2_x,gama_N1,gama_N2,sigma_y,A):
 
 	return height1, height2
 
-def func_inputData():
-	nw = 19
+# 参数输入----------------------------------------------------------------------------------- #
+if __name__ == '__main__':
+
+	nw = 7
 	d = 0.3
 	Rp = 0.5 
 	wx_origin = 2.95
@@ -116,12 +122,6 @@ def func_inputData():
 	ks = 10000000000000  # 弹簧刚度，指代卸扣边界（刚体）
 	ls0 = 0.05
 
-	return nw, d, Rp, wx_origin, wy_origin, sigma_y, ks, ls0
-
-# 参数输入----------------------------------------------------------------------------------- #
-if __name__ == '__main__':
-
-	nw, d, Rp, wx_origin, wy_origin, sigma_y, ks, ls0 = func_inputData()
 	dmin = func_choose_dmin(nw)
 	A = nw * np.pi*dmin**2/4  # 单肢截面面积
 
@@ -187,28 +187,19 @@ if __name__ == '__main__':
 	init_gama_N2_y = init_F2_y/(A*sigma_y)
 	F2_y, gama_N2_y = func_correct_gamaAndForce(my, init_gama_N2_y, gama_N1, init_F2_y, K1_y, L2_y, L0_y, sigma_y, A)
 
-	print('gama_N1_x=', gama_N1_x)
-	print('gama_N2_x=', gama_N2_x)
-
 	# 计算能量----------------------------------------------------------------------------------- # 
 
-	E1_x = K1_x * (L1_x-L0_x)**2 / 2
-	E2_x = K1_x * L2_x*(L1_x-L0_x) + K1_x*(L0_x**2-L1_x**2)/2 + K2_x*(L2_x-L1_x)**2 / 2
-	E1_y = K1_y * (L1_y-L0_y)**2 / 2
-	E2_y = K1_y * L2_y*(L1_y-L0_y) + K1_y*(L0_y**2-L1_y**2)/2 + K2_y*(L2_y-L1_y)**2 / 2
+	Energy1_x = K1_x * (L1_x-L0_x)**2 / 2
+	Energy2_x = K1_x * L2_x*(L1_x-L0_x) + K1_x*(L0_x**2-L1_x**2)/2 + K2_x*(L2_x-L1_x)**2 / 2
+	Energy1_y = K1_y * (L1_y-L0_y)**2 / 2
+	Energy2_y = K1_y * L2_y*(L1_y-L0_y) + K1_y*(L0_y**2-L1_y**2)/2 + K2_y*(L2_y-L1_y)**2 / 2
 
 	displacement = h2
 	Force = 4* np.sum(F2_x * h2 / L2_x, axis=0) + 4* np.sum(F2_y * h2 / L2_y,axis=0)
-	Energy = 4*np.sum(E2_x, axis=0) + 4*np.sum(E2_y, axis=0)
+	Energy = 4*np.sum(Energy2_x, axis=0) + 4*np.sum(Energy2_y, axis=0)
 
 	print('nw=', nw)
 	print('gama_N2=', gama_N2)
-	print('L1_x = ', L1_x)
-	print('lf1_x = ', lf1_x)
-	print('ls1_x = ', ls1_x)
-	print('L2_x = ', L2_x)
-	print('lf2_x = ', lf2_x)
-	print('ls2_x = ', ls2_x)
-	print('Force = ', Force)
-	print('displacement = ', displacement)
-	print('Energy = ', Energy)
+	print('Force=', Force)
+	print('displacement=', displacement)
+	print('Energy=', Energy)
