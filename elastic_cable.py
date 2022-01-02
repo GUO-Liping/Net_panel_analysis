c44ee0ccfb20cf7efb8b77639dbeb59859aff07e
郭立平 <liping0_0@my.swjtu.edu.cn>
2022-01-02 11:18:31 +0800

    Update elastic_cable.py

--- elastic_cable.py
+++ elastic_cable.py
@@ -15,7 +15,21 @@ Remark: 尚未解决的问题：
 '''
 
 import numpy as np
-from scipy.optimize import root
+from sympy import symbols
+from sympy import asinh, sqrt
+
+xi, eta, sigma, delta, gamma, tau, chi, phi, beta = symbols('xi eta sigma delta gamma tau chi phi beta')
+psi_i, Psi_i, Psi_i_1, Psi_N, sigma_i = symbols('psi_i Psi_i Psi_i_1 Psi_N sigma_i')
+
+f1_1 = gamma/chi - beta
+f1_2 = asinh(phi/chi) - asinh((phi-Psi_N-1)/chi)
+f1_3 = asinh((phi-Psi_i-sigma_i)/chi) - asinh((phi-Psi_i_1-sigma_i)/chi)
+f1 = -f1_1 + f1_2 + f1_3
+
+f2_1 = delta
+f2_2 = beta*(phi-1/2) + sqrt(phi**2+chi**2) - sqrt((phi-Psi_N-1)**2+chi**2)
+f2_3 = beta*psi_i*(sigma_i-1) + sqrt((phi-Psi_i-sigma_i)**2+chi**2) - sqrt((phi-Psi_i_1-sigma_i)**2+chi**2)
+f2 = -f2_1 + f2_2 + f2_3
 
 sigma_array = s_array / L_0
 psi_array = F_array / W
