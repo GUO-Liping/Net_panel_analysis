f7197a68d2da19e81cb773a51a84894d474cfa9c
郭立平 <liping0_0@my.swjtu.edu.cn>
2022-01-03 23:11:07 +0800

    Update elastic_cable.py

--- elastic_cable.py
+++ elastic_cable.py
@@ -15,24 +15,39 @@ Remark: 尚未解决的问题：
 '''
 
 import numpy as np
-from sympy import symbols
-from sympy import asinh, sqrt
+from sympy import symbols, Array
+from sympy import asinh, sqrt, diff, Sum
 
 xi, eta, sigma, delta, gamma, tau, chi, phi, beta = symbols('xi eta sigma delta gamma tau chi phi beta')
-psi_i, Psi_i, Psi_i_1, Psi_N, sigma_i = symbols('psi_i Psi_i Psi_i_1 Psi_N sigma_i')
+psi_i, Psi_i, Psi_i_1, sigma_i = symbols('psi_i Psi_i Psi_i_1 sigma_i')
+i,j = symbols('i j', integer=True)
+W, F1, F2, F3, F4, F5, F6, F7 = symbols('W F1 F2 F3 F4 F5 F6 F7')
+F = Array([F1, F2, F3, F4, F5, F6, F7])
+#i = 1
+N = len(F)
+psi = F/W
+Psi_N = Sum(psi[j], (j,0,N-1))
+Psi_i = Sum(psi[j], (j,0,i-1))
+Psi_i_1 = Sum(psi[j], (j,0,i-2))
+#Psi_i = Psi_i.doit()
+#Psi_i_1 = Psi_i_1.doit()
+#Psi_N = Psi_N.doit()
+#Psi_i_1
 
 f1_1 = gamma/chi - beta
 f1_2 = asinh(phi/chi) - asinh((phi-Psi_N-1)/chi)
-f1_3 = asinh((phi-Psi_i-sigma_i)/chi) - asinh((phi-Psi_i_1-sigma_i)/chi)
+f1_3 = Sum(asinh((phi-Psi_i-sigma_i)/chi) - asinh((phi-Psi_i_1-sigma_i)/chi), (i,0,N-1))
 f1 = -f1_1 + f1_2 + f1_3
+f1
 
 f2_1 = delta
 f2_2 = beta*(phi-1/2) + sqrt(phi**2+chi**2) - sqrt((phi-Psi_N-1)**2+chi**2)
-f2_3 = beta*psi_i*(sigma_i-1) + sqrt((phi-Psi_i-sigma_i)**2+chi**2) - sqrt((phi-Psi_i_1-sigma_i)**2+chi**2)
+f2_3 = Sum(beta*psi_i*(sigma_i-1) + sqrt((phi-Psi_i-sigma_i)**2+chi**2) - sqrt((phi-Psi_i_1-sigma_i)**2+chi**2), (i,0,N))
 f2 = -f2_1 + f2_2 + f2_3
+f2
 
-sigma_array = s_array / L_0
-psi_array = F_array / W
-psi_N = psi_array[-1]
+diff_f1_chi = diff(f1,chi)
+diff_f1_chi
 
-T_s = np.sqrt((Y_0 - sum_F - W*s/L_0)**2 + X**2)
+aaa = diff_f1_chi.subs(i,2)
+print(aaa.doit())
